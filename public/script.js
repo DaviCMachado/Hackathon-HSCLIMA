63beef9319800cfbcc425ecd2490b2ece223f2c4
luizebaldoni <oliveira.luize@acad.ufsm.br>
2024-05-25 17:18:27 -0300

    atualização de front-end: rotas estaticas arquivos externos e navbar

---
+++ public/script.js
@@ -0,0 +1,38 @@
+var map = L.map('map').setView([-29, -51], 13);
+
+	var planetLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(map);
+
+	planetLayer.addTo(map)
+
+	let qtd = 0
+	for(let i=0;i<100;i++){
+		let lat = -51 + 0.5 *i
+		var marker = L.marker([-29, lat])
+		qtd = qtd+1
+		var popup = marker.bindPopup(qtd.toString()) //COLOCAR REFERENCIA PARA DADOS DO BANCO de dados
+		marker.addTo(map)
+
+
+
+	function fetchData() {
+		fetch('/data')
+			.then(response => response.json())
+			.then(data => {
+				data.forEach(item => {
+					let marker = L.marker([-29 + item.codRegiao * 0.01, -51]);
+					marker.bindPopup(`Produto: ${item.codProduto}, Quantidade: ${item.quantidade}`);
+					marker.addTo(map);
+				});
+			})
+			.catch(error => console.error('Erro ao buscar dados:', error));
+	}
+
+	// Fetch data every 10 seconds
+	fetchData();
+	setInterval(fetchData, 10000);
+
+
+
+	}
